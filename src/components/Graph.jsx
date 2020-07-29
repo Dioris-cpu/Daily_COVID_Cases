4081c8e71cc213c32806ba4ba4a1e06cf7cb48c9
Dioris Diaz <diorid@gmail.com>
2020-07-29 18:13:00 -0400

    css added

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -64,32 +64,30 @@ function Graph() {
     runCall();
   }, []);
   return (
-    
-          <div className="chart-wraper">
-            {isLoading ? (
-              <div>loading...</div>
-            ) : (
-              <LineChart
-                width={800}
-                height={400}
-                data={data}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="data" stroke="#82ca9d" />
-              </LineChart>
-            )}
-          </div>
-       
+    <div className="chart-wraper">
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        <LineChart
+          width={800}
+          height={400}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="data" stroke="#82ca9d" />
+        </LineChart>
+      )}
+    </div>
   );
 }
 
--- src/components/about.js
+++ src/components/about.js
@@ -1,20 +1,39 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 function About() {
-    return (
-        <Container>
-        <Row>
-          <Col></Col>
-          <Col xs={6}> (wider)</Col>
-          <Col></Col>
-        </Row>
-        <Row>
-          <Col></Col>
-          <Col xs={5}>(wider)</Col>
-          <Col></Col>
-        </Row>
-      </Container>
-    );
-  }
-  export default About;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          {/* <h5>Social Distancing Tips</h5>
+          <p>
+            Before going out, know and follow the guidance from local public
+            health authorities where you live. Shop by mail rather than
+            in-person when possible. Disinfect mail-order retail packages with
+            an alcohol-based sanitizer spray or wipe as you bring them into your
+            house.
+          </p> */}
+        </Col>
+        <Col xs={6}>
+          <h5> COVID Cases Continue to Spread</h5>
+          <p>
+            Information from the ongoing COVID-19 pandemic suggests that this
+            virus is spreading more efficiently than influenza, but not as
+            efficiently as measles, which is highly contagious. In general, the
+            more closely a person interacts with others and the longer that
+            interaction, the higher the risk of COVID-19 spread. The graph below
+            displays the number of daily cases in the US.
+          </p>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={5}></Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+}
+export default About;
